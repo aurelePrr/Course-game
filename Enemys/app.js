d6af53bda5a849ac192f21ce527b2ef1418c7ca4
aurelePrr <aurele-poirier@hotmail.fr>
2022-12-22 11:36:27 +0100

    ravens add + create enemy folder

---
+++ Enemys/app.js
@@ -0,0 +1,27 @@
+/** @type {HTMLCanvasElement} */
+
+class Enemy {
+    constructor(){
+        this.x = 0
+        thys.y = 0 
+        this;width = 100
+        this.height = 200
+    }
+    update(){
+
+    }
+    drax(){
+
+    }
+}
+
+class Ghost extends Enemy {
+    constructor(){
+        super()
+        this.image = new Image
+    }
+}
+
+const enemy1 = new Ghost()
+enemy1.update()
+enemy1.draw()
